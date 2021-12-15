08492caf8e679dbe5d2b5a8e62364bcd56fa9627
liushangyang <liushangyang@hikvision.com.cn>
2021-12-15 15:05:10 +0800

    feat: init template

---
+++ pages/index/mock.js
@@ -0,0 +1,58 @@
+export const goodList = [
+    {
+        id: '1',
+        imgUrl: '../../assets/images/default.png',
+        name: 'item1',
+        price: 20,
+        description: 'description'
+    },
+    {
+        id: '2',
+        imgUrl: '../../assets/images/default.png',
+        name: 'item2',
+        price: 20,
+        description: 'description'
+    },
+    {
+        id: '3',
+        imgUrl: '../../assets/images/default.png',
+        name: 'item3',
+        price: 20,
+        description: 'description'
+    },
+    {
+        id: '4',
+        imgUrl: '../../assets/images/default.png',
+        name: 'item4',
+        price: 20,
+        description: 'description'
+    },
+    {
+        id: '5',
+        imgUrl: '../../assets/images/default.png',
+        name: 'item5',
+        price: 20,
+        description: 'description'
+    },
+    {
+        id: '6',
+        imgUrl: '../../assets/images/default.png',
+        name: 'item6',
+        price: 20,
+        description: 'description'
+    },
+    {
+        id: '7',
+        imgUrl: '../../assets/images/default.png',
+        name: 'item7',
+        price: 20,
+        description: 'description'
+    },
+    {
+        id: '8',
+        imgUrl: '../../assets/images/default.png',
+        name: 'item8',
+        price: 20,
+        description: 'description'
+    },
+]
