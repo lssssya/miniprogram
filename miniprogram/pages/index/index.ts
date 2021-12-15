b643b0f6790a810c528d45bf6f74c6867c7dc80a
liushangyang <liushangyang@hikvision.com.cn>
2021-12-15 15:31:35 +0800

    chore: update to ts

--- miniprogram/pages/index/index.ts
+++ miniprogram/pages/index/index.ts
@@ -1,8 +1,8 @@
-// index.js
+// index.ts
 import { goodList } from './mock'
 
 // 获取应用实例
-const app = getApp()
+const app = getApp<IAppOption>()
 
 Page({
   data: {
@@ -24,13 +24,14 @@ Page({
     })
   },
   onLoad() {
+    // @ts-ignore
     if (wx.getUserProfile) {
       this.setData({
         canIUseGetUserProfile: true
       })
     }
   },
-  getUserProfile(e) {
+  getUserProfile() {
     // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
     wx.getUserProfile({
       desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
@@ -43,7 +44,7 @@ Page({
       }
     })
   },
-  getUserInfo(e) {
+  getUserInfo(e: any) {
     // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
     console.log(e)
     this.setData({
